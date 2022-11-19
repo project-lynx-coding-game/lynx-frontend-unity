93cc5ff77e4a63c8b0051fc99591a81e7ecc4444
blazej-smorawski <blazej.smorawski@gmail.com>
2022-11-19 17:03:17 +0100

    Update to `NextScene` protocol

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class CreateScene : Action
+public class NextScene : Action
 {
-    public CreateScene(string propertiesJson) : base()
+    public NextScene(string propertiesJson) : base()
     {
 
     }
